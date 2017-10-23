4c467a09b09643e885ef8f856fa0e9424457f3f7
SRI NARAYANAN T <srinarayana@srinarayana-m01.vmware.com>
2017-10-24 00:20:32 +0530

    testcaseresource

--- go/src/github.com/srinarayanant/terraform-provider-vcloud-director/go/src/vcd/provider/resource_catalog_item_media_test.go
+++ go/src/github.com/srinarayanant/terraform-provider-vcloud-director/go/src/vcd/provider/resource_catalog_item_media_test.go
@@ -1,9 +1,56 @@
 package provider;
-import "testing"
+import (
+	//"fmt"
+	"testing"
+
+	//"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform/helper/schema"
+
+)
+var testAccProvider *schema.Provider
+var testAccProviders map[string]terraform.ResourceProvider
+
+func TestAccResourceCatalogItemMedia(t *testing.T) {
+	
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCatalogItemDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCatalogItem_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCatalogItelUpload("c1"),
+					
+				),
+			},
+		},
+	})
+}
 
 
-func TestResourceCatalogItemMediaCreate(t *testing.T){
+func testAccCheckCatalogItelUpload(catalogName string) resource.TestCheckFunc {
+	return testAccCheckCatalogItemDestroy
+}
+func testAccCheckCatalogItemDestroy(s *terraform.State) error {
+	
 
-	//t.Errorf("Sum was incorrect, got: %d, want: %d.", 12, 10)
+	return nil
+
+
+}
+
+const testAccCatalogItem_basic = `
+resource "vcloud-director_catalog_item_media" "item1" {
+	item_name = "item1"
+	catalog_name= "c3"
+	file_path="/home/file.iso"
+}
+`
+
+func testAccPreCheck(t *testing.T) {
 
 }
